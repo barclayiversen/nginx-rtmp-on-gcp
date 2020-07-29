5e49e4e61aa98db310abbd249208260c38dcb55d
Spracto <barclayiversen@gmail.com>
2020-07-29 15:26:35 -0700

    Add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+resource "google_compute_instance" "default" {
+  name         = "nginx-rtmp-tf"
+  machine_type = "g1-small"
+  zone         = "us-west1-b"
+  project = "mixfreely"
+
+  tags = ["obs", "rtmp", "http-server", "https-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "projects/mixfreely/global/images/family/nginx-rtmp"
+      type = "pd-ssd"
+    }
+
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+
+    }
+  }
+
+  metadata = {}
+
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
